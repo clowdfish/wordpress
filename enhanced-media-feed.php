21ab63db20ccd742337f6c2207b2fe5e1aca2269
Sascha Gros <gros.sascha@gmail.com>
2014-07-14 15:43:00 +0200

    Initial commit

---
+++ enhanced-media-feed.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:   Enhanced Media RSS Feed
+ * Plugin URI:    http://www.clowdfish.com
+ * Description:   A plugin to enhance the default RSS Feed to include media and filter options.
+ * Version:       1.0
+ * Author:        Sascha Gros
+ * Author URI:    http://www.clowdfish.com
+ */
+ 
+ defined('ABSPATH') or die("No script kiddies please!");
+ 
+ 
+ 
+?>
+
